d82c1a4e86edc3ac77b057d985930247c4e17c4f
Vovka95 <volodimir.potskiy@gmailcom>
2019-03-07 11:07:05 +0200

    Updates

---
+++ precache-manifest.d7b6f515a92e793e7854246f5f7d74bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f7ae3534798a48e5fe0",
+    "url": "/ToDo/static/css/main.ff17d676.chunk.css"
+  },
+  {
+    "revision": "1f7ae3534798a48e5fe0",
+    "url": "/ToDo/static/js/main.1f7ae353.chunk.js"
+  },
+  {
+    "revision": "3c3b113506d4277ee49a",
+    "url": "/ToDo/static/js/runtime~main.3c3b1135.js"
+  },
+  {
+    "revision": "3afe8c4a508c3dd12967",
+    "url": "/ToDo/static/css/2.90e8b122.chunk.css"
+  },
+  {
+    "revision": "3afe8c4a508c3dd12967",
+    "url": "/ToDo/static/js/2.3afe8c4a.chunk.js"
+  },
+  {
+    "revision": "fb72e0211cc9459c567f1058e1555c58",
+    "url": "/ToDo/index.html"
+  }
+];
